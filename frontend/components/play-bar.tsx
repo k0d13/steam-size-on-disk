9efd3b832ab8cc5b3e68a0760e10fcfe535345c6
k0d13 <40654585+k0d13@users.noreply.github.com>
2026-05-15 10:56:30 +1200

    Add tooltip with more info content

--- frontend/components/play-bar.tsx
+++ frontend/components/play-bar.tsx
@@ -1,26 +1,105 @@
-import { findClassModule, findModuleExport, IconsModule } from "@steambrew/client";
-import Steam from "../steam.js";
+import {
+  findClassModule,
+  findModuleDetailsByExport,
+  findModuleExport,
+  IconsModule,
+} from "@steambrew/client";
 
 const PlayBar = findClassModule((m) => m.GameStat) as Record<string, string>;
 export const PlayBarClasses = PlayBar;
 
 const formatBytes = //
   findModuleExport((e) => e?.toString?.()?.includes('"Tera"'));
+const t = findModuleExport((e) => e?.toString?.()?.match(/\.LocalizeString\([a-z]\)/g));
+const Tooltip = findModuleDetailsByExport(
+  (m) =>
+    m?.toString?.()?.includes(`divProps`) &&
+    m?.toString?.()?.includes(`tooltipProps`) &&
+    m?.toString?.()?.includes(`toolTipContent`) &&
+    m?.toString?.()?.includes(`tool-tip-source`),
+)?.[1];
+const getNavigator = findModuleExport((e) =>
+  e?.toString?.()?.includes('"No global navigator context found!"'),
+);
 
-export function SizeOnDisk({ sizeOnDisk }: { sizeOnDisk: Steam.AppOverview["size_on_disk"] }) {
+interface SizeOnDiskProps {
+  appId: number;
+  driveName: string;
+  folderLabel: string;
+  folderPath: string;
+  dlcSize: number;
+  workshopSize: number;
+  shaderSize: number;
+  totalSize: number;
+}
+
+export function SizeOnDisk({
+  appId,
+  driveName,
+  folderLabel,
+  dlcSize,
+  workshopSize,
+  shaderSize,
+  totalSize,
+}: SizeOnDiskProps) {
+  // The only "Size on Disk" translation is for sorting, so while it makes sense still in English,
+  // it's not ideal for other languages, so we just use the app size instead in those cases
+  const sizeOnDiskLabel = t("#Library_SortBySizeOnDisk");
+  const sizeLabel = t("#ContentManagement_AppSize");
+  const label = sizeOnDiskLabel === "Size on Disk" ? sizeOnDiskLabel : sizeLabel;
+
+  const navigator = getNavigator();
+  const appSize = totalSize - dlcSize - workshopSize - shaderSize;
+
+  const toolTipContent = (
+    <>
+      {t(
+        "#AppProperties_LocalFilesSizeOnDrive",
+        formatBytes(totalSize, 2),
+        `${folderLabel} (${driveName})`,
+      )}
+      <br />
+      <span style={{ textTransform: "capitalize" }}>{t("#AppType_Singular_2")}</span>:{" "}
+      {formatBytes(appSize, 2)}
+      {dlcSize > 0 && (
+        <>
+          <br />
+          {t("#ContentManagement_UsedByDLC")}: {formatBytes(dlcSize, 2)}
+        </>
+      )}
+      {workshopSize > 0 && (
+        <>
+          <br />
+          {t("#ContentManagement_UsedByWorkshop")}: {formatBytes(workshopSize, 2)}
+        </>
+      )}
+      {shaderSize > 0 && (
+        <>
+          <br />
+          {t("#ContentManagement_UsedByShaders")}: {formatBytes(shaderSize, 2)}
+        </>
+      )}
+    </>
+  );
+
   return (
-    <div className={`${PlayBar.GameStat} ${PlayBar.LastPlayed} Panel`}>
-      <div className={`${PlayBar.GameStatIcon} ${PlayBar.PlaytimeIcon}`}>
-        <IconsModule.HardDrive />
-      </div>
-      <div className={PlayBar.GameStatRight}>
-        <div className={PlayBar.PlayBarLabel}>
-          {Steam.LocalizationManager.LocalizeString("#Library_SortBySizeOnDisk")}
+    <Tooltip toolTipContent={toolTipContent}>
+      <div
+        data-size-on-disk
+        onClick={() => navigator?.AppProperties(appId, "localfiles")}
+        className={`${PlayBar.GameStat} ${PlayBar.LastPlayed} Panel`}
+        style={{ cursor: "pointer" }}
+      >
+        <div className={`${PlayBar.GameStatIcon} ${PlayBar.PlaytimeIcon}`}>
+          <IconsModule.HardDrive />
         </div>
-        <div className={`${PlayBar.PlayBarDetailLabel} ${PlayBar.LastPlayedInfo}`}>
-          {formatBytes(Number(sizeOnDisk), 2)}
+        <div className={PlayBar.GameStatRight}>
+          <div className={PlayBar.PlayBarLabel}>{label}</div>
+          <div className={`${PlayBar.PlayBarDetailLabel} ${PlayBar.LastPlayedInfo}`}>
+            {formatBytes(Number(totalSize), 2)}
+          </div>
         </div>
       </div>
-    </div>
+    </Tooltip>
   );
 }
--- frontend/renderers/library-app.tsx
+++ frontend/renderers/library-app.tsx
@@ -1,26 +1,39 @@
-import type Steam from "../steam.js";
-import { PlayBarClasses, SizeOnDisk } from "../components/play-bar.js";
-import { NON_STEAM_APP_APPID_MASK } from "../constants.js";
-import { querySelectorAll, renderComponent } from "../helpers.js";
-import logger from "../logger.js";
+import { NON_STEAM_APP_APPID_MASK, Steam } from "steambrew-utils";
+import { querySelectorAll, renderComponent } from "steambrew-utils/dom";
+import { logger } from "..";
+import { PlayBarClasses, SizeOnDisk } from "../components/play-bar";
 
-export async function patch(window: Window, app: Steam.AppOverview) {
-  if (app.appid >= NON_STEAM_APP_APPID_MASK) return;
+export async function patch(
+  window: Window,
+  folder: Steam.InstallFolder,
+  app: Steam.InstallFolderApp,
+) {
+  if (app.nAppID >= NON_STEAM_APP_APPID_MASK) return;
 
   logger.debug(
-    `Patching library app '${app.display_name}' to add size on disk`, //
+    `Patching library app '${app.strAppName}' to add size on disk`, //
     { window, app },
   );
 
   const parents = await querySelectorAll(window.document, `.${PlayBarClasses.GameStatsSection}`);
 
   for (const parent of parents) {
-    if (app.size_on_disk) {
-      const component = <SizeOnDisk sizeOnDisk={app.size_on_disk} />;
+    if (app.nUsedSize) {
+      const component = (
+        <SizeOnDisk
+          appId={app.nAppID}
+          driveName={folder.strDriveName}
+          folderLabel={folder.strUserLabel}
+          folderPath={folder.strFolderPath}
+          dlcSize={app.nDLCSize}
+          workshopSize={app.nWorkshopSize}
+          shaderSize={app.nShaderSize}
+          totalSize={app.nUsedSize}
+        />
+      );
       const element = renderComponent(component);
-      element.setAttribute("data-size-on-disk", "");
 
-      const existing = parent.querySelector(`[data-size-on-disk]`);
+      const existing = parent.querySelector("[data-size-on-disk]");
       if (existing) existing.replaceWith(element);
       else parent.appendChild(element);
     }
